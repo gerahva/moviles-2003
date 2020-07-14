8289877be82ab58069a818d105482658b4defe63
humbe <humbertova_mx@hotmail.com>
2020-07-14 17:52:41 -0500

    mi primer commit a github

--- src/main/java/com/unitec/humbert/ControladorPerfil.java
+++ src/main/java/com/unitec/humbert/ControladorPerfil.java
@@ -4,6 +4,7 @@ package com.unitec.humbert;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author humbe
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ControladorPerfil {
     
